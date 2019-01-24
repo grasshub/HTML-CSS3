0b4d20f69c4923541244adaf2ca8af8c391f2bcf
Hong Yu <umyuh1@gmail.com>
2019-01-24 02:15:11 -0500

    Commit changes.

--- js/process-page.js
+++ js/process-page.js
@@ -1,34 +1,34 @@
 {
-    "use strict";
+  "use strict";
 
-    // Add listener for button 
-    document.getElementById("helloButton").addEventListener( "click", function() {
-        alert("Hello World!");
-        const myTextNode = document.createTextNode("Some new words.");
-        // Append “Some new words.” to the page.
-        document.body.appendChild(myTextNode); 
-    });
+  // Add listener for button 
+  document.getElementById("helloButton").addEventListener("click", function () {
+    alert("Hello World!");
+    const myTextNode = document.createTextNode("Some new words.");
+    // Append “Some new words.” to the page.
+    document.body.appendChild(myTextNode);
+  });
 
-    document.getElementById("locationButton").addEventListener( "click", function() {
-        getLocation();
-    });
+  document.getElementById("locationButton").addEventListener("click", function () {
+    getLocation();
+  });
 
-    const location = document.querySelector("div#geoLocation p#location");
+  const location = document.querySelector("div#geoLocation p#location");
 
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(getCurrentPositionCallback);
-        } else {
-            location.innerHTML = "Geolocation is not supported in your browser!";
-        } 
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getCurrentPositionCallback);
+    } else {
+      location.innerHTML = "Geolocation is not supported in your browser!";
     }
+  }
 
-    function getCurrentPositionCallback(position) {
-        location.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude +
-        "<br>Speed: " + position.coords.speed +
-        "<br>Altitude: " + position.coords.altitude +
-        "<br>Accuracy: " + position.coords.accuracy;
-    }
+  function getCurrentPositionCallback(position) {
+    location.innerHTML = "Latitude: " + position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude +
+      "<br>Speed: " + position.coords.speed +
+      "<br>Altitude: " + position.coords.altitude +
+      "<br>Accuracy: " + position.coords.accuracy;
+  }
 
 };
